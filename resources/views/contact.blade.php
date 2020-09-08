df458a6bde3924d243cb2c184c469853e28407e2
Jordan Pickett <jordanepickett@gmail.com>
2020-09-08 15:50:26 -0600

    homepage/about change

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -14,32 +14,32 @@
                 <a href="https://www.instagram.com/whitnee.wright" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
             </div>
         </div>
-        <div class="row">
-            <h2>Print Prices</h2>
-            <h3>Suminagashi</h3>
-            <h3>Watercolor</h3>
-            <h3>Linework</h3>
-            <p style="font-weight: bold">Acid free smooth fine art paper</p>
-            <p>
-                8in x 10in $18<br>
-                9in x 12in $22<br>
-                11in x 14in $33<br>
-                12inx16in $40<br>
-                16in x 20in $65<br>
-                18in x 24in $90<br>
-                20in x 30in $125<br>
-            </p>
-            <h3>Alcohol Ink</h3>
-            <p style="font-weight: bold">Acid free satin paper</p>
-            <p>
-                8in x 10in $15<br>
-                9in x 12in $20<br>
-                11in x 14in $25<br>
-                12inx16in $30<br>
-                16in x 20in $50<br>
-                18in x 24in $70<br>
-                20in x 30in $100<br>
-            </p>
+        {{--<div class="row">--}}
+            {{--<h2>Print Prices</h2>--}}
+            {{--<h3>Suminagashi</h3>--}}
+            {{--<h3>Watercolor</h3>--}}
+            {{--<h3>Linework</h3>--}}
+            {{--<p style="font-weight: bold">Acid free smooth fine art paper</p>--}}
+            {{--<p>--}}
+                {{--8in x 10in $18<br>--}}
+                {{--9in x 12in $22<br>--}}
+                {{--11in x 14in $33<br>--}}
+                {{--12inx16in $40<br>--}}
+                {{--16in x 20in $65<br>--}}
+                {{--18in x 24in $90<br>--}}
+                {{--20in x 30in $125<br>--}}
+            {{--</p>--}}
+            {{--<h3>Alcohol Ink</h3>--}}
+            {{--<p style="font-weight: bold">Acid free satin paper</p>--}}
+            {{--<p>--}}
+                {{--8in x 10in $15<br>--}}
+                {{--9in x 12in $20<br>--}}
+                {{--11in x 14in $25<br>--}}
+                {{--12inx16in $30<br>--}}
+                {{--16in x 20in $50<br>--}}
+                {{--18in x 24in $70<br>--}}
+                {{--20in x 30in $100<br>--}}
+            {{--</p>--}}
         </div>
     </div>
 @endsection
